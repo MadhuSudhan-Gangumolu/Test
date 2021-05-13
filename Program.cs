5743577173b10415391f0bf57959c23f8e1be2be
Madhu Gangumolu <Madhu Gangumolu@COGNINE-L87>
2021-05-13 20:27:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+namespace Test
+{
+    
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var s = "String";
+        }
+    }
+   
+    public class A
+    {
+        public A()
+        {
+
+            Console.WriteLine("This is base class");
+        }
+        public virtual string getname()
+        {
+            return "madhu";
+        }
+        public virtual void run()
+        {
+            
+        }
+        
+    }
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("This is derived class");
+        }
+        public void display()
+        {
+            Console.WriteLine(base.getname());
+        }
+        public sealed override void run()
+        {
+
+        }
+    }
+    class C : A
+    {
+        public override void run()
+        {
+            
+        }
+
+    }
+    
+
+}
